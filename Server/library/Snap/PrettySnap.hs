2b3ffc4437b9fc00b4c9e5a0f2a0090480633ecf
Tobias Wochinger <wochinge@hm.edu>
2015-05-04 12:43:31 +0200

    setJSONHEader implementiert

--- Server/library/Snap/PrettySnap.hs
+++ Server/library/Snap/PrettySnap.hs
@@ -4,8 +4,10 @@
 module Snap.PrettySnap where
 
 import Data.Aeson
+import Data.Maybe
 import Snap.Core
 import Snap.Snaplet
+import qualified Data.ByteString.Lazy as B
 
 -- | Sets the status code of the server response.
 setStatusCode :: MonadSnap m 
@@ -13,11 +15,21 @@ setStatusCode :: MonadSnap m
               -> m ()
 setStatusCode code = modifyResponse . setResponseCode $ code
 
---setJSONHeader :: (MonadSnap m) => (Response -> Response) -> m ()
---setJSONHeader = modifyResponse . setHeader "Content-Type" "application/json"
-
--- | Sets the body a server response.
+-- | Sets the content-type of the answer to application/json
+setJSONHeader :: MonadSnap m => m ()
+setJSONHeader = modifyResponse header
+    where header = setHeader "Content-Type" "application/json"
+    
+-- | Sets the body of a server response.
 setBody :: (ToJSON a, MonadSnap m) 
                 => a -- ^ object which should be sent. Needs method toJSON to convert object to a JSON
                 -> m ()
-setBody object = writeLBS . encode $ object
+setBody object = do
+    setJSONHeader
+    writeLBS . encode $ object
+
+-- | Decodes JSON body back to data.
+decodeBody :: FromJSON a 
+           => B.ByteString -- ^ body 
+           -> a            -- ^ decoded data
+decodeBody body = fromJust $ decode body
